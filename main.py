e6645e5aedda5ae43c8b6c0090589606636b7886
anelissaespin <anelissa2006@gmail.com>
2026-02-10 18:12:15 -0400

    uso del sdk

---
+++ main.py
@@ -0,0 +1,19 @@
+from game import Game
+from arcade_machine_sdk import GameMeta
+import pygame
+
+if not pygame.get_init():
+    pygame.init()
+
+metadata = (GameMeta()
+            .with_title("Super Mario Bross")
+            .with_description("Juego de prueba")
+            .with_release_date("10/02/2026")
+            .with_group_number(1)
+            .add_tag("Plataforma")
+            .add_author("Anelissa Espin, Erick Gomez, abelardo Drika"))
+
+game = Game(metadata)
+
+if __name__ == "__main__":
+    game.run_independently()
